4e3d097e2e553b9b63fedf03f35eef4a2096db79
kang <kyungohkang126@gmail.com>
2026-05-18 04:18:47 +0900

    보안 헤더 설정 - helmet 사용

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   app.enableCors({
     origin: process.env.ALLOWED_ORIGIN,
     methods: ['POST', 'PUT', 'DELETE'],
