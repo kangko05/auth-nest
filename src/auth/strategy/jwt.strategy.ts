1ff5cb0c4fa7d6f47172307699d977de80b4b20e
kang <kyungohkang126@gmail.com>
2026-06-08 16:39:18 +0900

    fix: 버그 발견 및 수정
    버그 내용:
        tokenValidAfter 적용 시 iat < tokenValidAfter로 비교하고 있었는데
        jwt의 iat와 mysql의 datetime 단위가 각각 초, 밀리초 단위로 달라서 오차
        발생
    
    수정 사항:
    iat <= tokenValidAfter 로 tokenValidAfter가 반올림 되어 비교함으로 초
    단위로 계산 및 비교하도록 수정

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,34 +2,34 @@ import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AppException, ErrorCode } from '../../common/exception.filter';
 import { UserRole } from '../../users/entities/user.entity';
 import { UsersService } from '../../users/users.service';
-import { AppException, ErrorCode } from '../../common/exception.filter';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    private readonly configService: ConfigService,
-    @Inject(UsersService) private readonly userService: UsersService,
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.get('jwt.secret') as string,
-      algorithms: ['HS256'],
-    });
-  }
+    constructor(
+        private readonly configService: ConfigService,
+        @Inject(UsersService) private readonly userService: UsersService,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: configService.get('jwt.secret') as string,
+            algorithms: ['HS256'],
+        });
+    }
 
-  async validate(payload: { sub: string; role: UserRole; iat: number }) {
-    const user = await this.userService.findByUserId(payload.sub);
+    async validate(payload: { sub: string; role: UserRole; iat: number }) {
+        const user = await this.userService.findByUserId(payload.sub);
 
-    if (!user) throw new AppException(ErrorCode.USER_NOT_FOUND);
+        if (!user) throw new AppException(ErrorCode.USER_NOT_FOUND);
 
-    const tokenValidAfter = user.tokenValidAfter;
+        const tokenValidAfter = user.tokenValidAfter;
 
-    if (tokenValidAfter && payload.iat < tokenValidAfter.getTime() / 1000)
-      throw new UnauthorizedException();
+        if (tokenValidAfter && payload.iat <= tokenValidAfter.getTime() / 1000)
+            throw new UnauthorizedException();
 
-    return { id: payload.sub, role: payload.role };
-  }
+        return { id: payload.sub, role: payload.role };
+    }
 }
