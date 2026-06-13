59783f54f8170631c908d79b4f3c145c9b2b69ed
kang <kyungohkang126@gmail.com>
2026-06-13 18:55:15 +0900

    포매팅

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -4,28 +4,28 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MailService } from './mail.service';
 
 @Module({
-    imports: [
-        MailerModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: (configService: ConfigService) => {
-                const mailEnabled = configService.get("mail.enabled");
-                const enabledTrans = {
-                    transport: {
-                        host: 'smtp.gmail.com',
-                        port: 587,
-                        auth: {
-                            user: configService.get('mail.user'),
-                            pass: configService.get('mail.pass'),
-                        },
-                    }
-                };
-
-                return mailEnabled ? enabledTrans : { transport: { jsonTransport: true } };
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const mailEnabled = configService.get("mail.enabled");
+        const enabledTrans = {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            auth: {
+              user: configService.get('mail.user'),
+              pass: configService.get('mail.pass'),
             },
-        }),
-    ],
-    providers: [MailService],
-    exports: [MailService],
+          }
+        };
+
+        return mailEnabled ? enabledTrans : { transport: { jsonTransport: true } };
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule { }
